d90c285c1b73faf65147802e5c0625b4c3262044
timmbach <follow.timothy@gmail.com>
2023-10-15 03:45:31 +0100

    added authentication  code

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
+// import express from "express";
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 import colors from "colors";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -11,13 +14,27 @@ mongoose.connect(process.env.MONGODB_URI).then((conn) => {
     `Connected to MongoDB server ${conn.connection.host}`.cyan.underline.bold
   );
 });
-
 const app = express();
+// app.use(cors);
+app.use(express.json());
 
-app.use(cors);
+const PORT = 5000;
 
-const PORT = 3000;
+app.use("/api/user", userRouter);
+// auth
+app.use("/api/auth", authRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// error middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
+app.listen(5000, () => {
+  console.log(`Server running on port ${PORT} 5000`);
 });
