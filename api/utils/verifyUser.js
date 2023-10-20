13c2df5dbb0042b82521bd7d4f18c1ece7b2baa1
timmbach <follow.timothy@gmail.com>
2023-10-20 14:01:15 +0100

    commit before version rewind

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -7,7 +7,7 @@ export const verifyToken = (req, res, next) => {
   if (!token) return next(errorHandler(401, "Unauthorized"));
 
   jwt.verify(token, process.env.JWT_SECRET, (error, user) => {
-    if (error) return next(errorHandler(403, "FOrbidden"));
+    if (error) return next(errorHandler(403, "Forbidden"));
 
     req.user = user;
     next();
