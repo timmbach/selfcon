9dda5fd67fe773d8e6e0f740baeabdab289d4d01
timmbach <follow.timothy@gmail.com>
2023-10-23 00:36:48 +0100

    removed duplicate import of cookie parser

--- api/index.js
+++ api/index.js
@@ -7,7 +7,6 @@ import colors from "colors";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
-import cookieParser from "cookie-parser";
 
 dotenv.config();
 
