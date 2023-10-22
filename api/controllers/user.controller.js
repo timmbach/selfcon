0291703918b6bbe623b319123366ce63b0dee0e6
timmbach <follow.timothy@gmail.com>
2023-10-22 23:43:02 +0100

    added user update api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,35 @@
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcrypt from "bcryptjs";
+
 export const test = (req, res) => {
   res.send("Hello world");
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "Can't validate account owner"));
+
+  try {
+    if (req.body.password) {
+      req.body.password = bcrypt.hashSync(password, 10);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          profileImg: req.body.profileImg,
+        },
+      },
+      { new: true }
+    );
+
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
